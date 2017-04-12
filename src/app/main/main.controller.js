0d7fe7973da99a077f57867bb6939a3136595fa6
Jon Manock <jonmanock@gmail.com>
2017-04-12 13:20:12 -0400

    nothing is working right

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,16 +1,37 @@
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
+//
+//   angular
+//     .module('sRetro')
+//     .controller('MainController', MainController);
+//
+//
+//   /** @ngInject */
+//   function MainController($scope, $interval){
+//     $scope.minDate = new Date();
+//   }// End `MainController`
+//
+// })();
 
-  angular
-    .module('sRetro')
-    .controller('MainController', MainController);
-
-
-  /** @ngInject */
-  function MainController($scope, $interval){
-    $scope.minDate = new Date();
-  }// End `MainController`
-
-
-
-})();
+angular.module('sRetro', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+.controller('MainController', function(){
+  this.myDate = new Date();
+  this.isOpen = false;
+});
+angular
+  .module('sRetro')
+  .config(function($mdDateLocaleProvider){
+    $mdDateLocaleProvider.firstDayOfWeek = 1;
+    $mdDateLocaleProvider.formatDate = function(date){
+      var injector = angular.injector(['ng']), $filter = injector.get('$filter');
+      var currWeek;
+      if(date === undefined){
+        date = new Date();
+      }
+      var currWeek = 'Week '+$filter('date')(date, 'ww');
+      return currWeek;
+    };
+    $mdDateLocaleProvider.weekNumberFormatter = function(weekNumber){
+      return 'Semaine '+weekNumber;
+    };
+  });
