9244f27afb438ac11f559cd6853b8cd8fa433ab5
Jon Manock <jonmanock@gmail.com>
2017-04-13 09:25:39 -0400

    stupid date picker

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,37 +1,14 @@
-// (function() {
-//   'use strict';
-//
-//   angular
-//     .module('sRetro')
-//     .controller('MainController', MainController);
-//
-//
-//   /** @ngInject */
-//   function MainController($scope, $interval){
-//     $scope.minDate = new Date();
-//   }// End `MainController`
-//
-// })();
+(function() {
+  'use strict';
 
-angular.module('sRetro', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-.controller('MainController', function(){
-  this.myDate = new Date();
-  this.isOpen = false;
-});
-angular
-  .module('sRetro')
-  .config(function($mdDateLocaleProvider){
-    $mdDateLocaleProvider.firstDayOfWeek = 1;
-    $mdDateLocaleProvider.formatDate = function(date){
-      var injector = angular.injector(['ng']), $filter = injector.get('$filter');
-      var currWeek;
-      if(date === undefined){
-        date = new Date();
-      }
-      var currWeek = 'Week '+$filter('date')(date, 'ww');
-      return currWeek;
-    };
-    $mdDateLocaleProvider.weekNumberFormatter = function(weekNumber){
-      return 'Semaine '+weekNumber;
-    };
-  });
+  angular
+    .module('sRetro')
+    .controller('MainController', MainController);
+
+
+  /** @ngInject */
+  function MainController($scope, $interval){
+    $scope.minDate = new Date();
+  }// End `MainController`
+
+})();
