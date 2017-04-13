96d03868e5e70d36c0b8da47f6e2eaaf20a9de35
Jon Manock <jonmanock@gmail.com>
2017-04-13 10:26:20 -0400

    updates

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('sRetro', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
