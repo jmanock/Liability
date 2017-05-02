64acfe17a5bf96298e8edc90d491ea93a30a531d
Jon Manock <jonmanock@gmail.com>
2017-05-02 16:28:15 -0400

    back to the basics

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+document.addEventListener('DOMContentLoaded', function(){
+  $(document).ready(function(){
+    $('form').hide();
+    $('.datepicker').datepicker({
+      prevText:'<i class="fa fa-fw fa-angle-left"></i>',
+      nextText:'<i class="fa fa-fw fa-angle-right"></i>',
+      beforeShowDay:function(date){
+        var day = date.getDay();
+        return [day !=0 && day!=1, ''];
+      }
+    });// End `DatePicker`
+    $('.datepicker').on('change',function(e){
+      e.preventDefault();
+      var date = $(this).val();
+      $('form').show();
+      $('.date').append(date);
+    });// End `Change`
+  });// End `Ready`
+});// End `Event Listener`
