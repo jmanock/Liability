9e5610dcbd1a66722c4ff485b44f65023006b844
Jon Manock <jonmanock@gmail.com>
2017-06-20 11:12:23 -0400

    setting up the work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  /*
+    ~ Styles Name
+    ~ Time of Appointment every 15 mins
+    ~ Up to two Appointments per time
+    ~ Have to check and see if time is avaible
+  */
+  $('#date').hide();
+  $('.datepicker').datepicker({
+    prevText:'<i class="fa fa-fw fa-angle-left"></i>',
+    nextText:'<i class="fa fa-fw fa-angle-right"></i>'
+  });
+
+  $('.datepicker').on('change', function(e){
+    e.preventDefault();
+    var date = $(this).val();
+    $('.send').prop('disabled', false);
+    $('#showDate').text('Date: '+date);
+    $('#date').val(date);
+    $('#date').hide();
+  });
+});
