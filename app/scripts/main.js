563ebee1e9291af9a426ef838a145370f03b679a
Jon Manock <jonmanock@gmail.com>
2017-06-22 17:31:50 -0400

    seting up some of the back end

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,3 +38,22 @@ $(document).ready(function(){
 
   startUp();
 });
+
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1p9t8s8-WC5F6pnxJs3VF0RH5s9gGzFFN6kzL2tjFBBI/pubhtml?gid=0&single=true';
+
+function init(){
+  Tabletop.init({
+    key:publicSpreadsheetUrl,
+    callback:showInfo,
+    simpleSheet:true
+  });
+}
+
+function showInfo(data, tabletop){
+  // Needs to hide times that are taken that day
+  data.forEach(function(x){
+    //console.log(x);
+  });
+}
+
+window.addEventListener('DOMContentLoaded', init);
