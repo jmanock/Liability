94cbf0842dafc5ab931ad8bd2c41763c4e2cccab
jManock <jonmanock@gmail.com>
2018-07-19 14:33:14 -0400

    start

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('liability', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
