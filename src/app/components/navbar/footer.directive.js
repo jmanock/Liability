0669394728b12a6a799488829264357ef2fd5126
jManock <jonmanock@gmail.com>
2018-07-22 00:21:00 -0400

    p1

---
+++ src/app/components/navbar/footer.directive.js
@@ -0,0 +1,24 @@
+(function(){
+  'use strict';
+  angular
+    .module('liability')
+    .directive('acmeFooter', acmeFooter);
+
+  function acmeFooter(){
+    var directive = {
+      restrict:'E',
+      templateUrl:'app/components/navbar/footer.html',
+      scope:{},
+      controller:FooterController,
+      controllerAs:'vm',
+      bindToController:true
+    };
+    return directive;
+    function FooterController($window){
+      var vm = this;
+      vm.Top = function(){
+        $window.scrollTo(0,0);
+      }
+    }
+  }
+})();
